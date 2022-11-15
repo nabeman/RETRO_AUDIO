c1bdfca354c59cd6c2baa7a5981bd54954aaeace
nabeman <tkmcoins20@gmail.com>
2022-11-15 14:56:17 +0900

    入力によって処理を変える

--- main.js
+++ main.js
@@ -6,14 +6,30 @@ function start_input(e){
     if(!state){
         document.addEventListener("keypress", keypress, false);
         state = true;
+        console.log("入力を受け付けています");
     }else{
         document.removeEventListener("keypress", keypress, false);
         state = false;
-        console.log("入力を停止しました")
+        console.log("入力を停止しました");
     }
 }
 
 function keypress(e){
-    console.log(e.key);
+    switch(e.key){
+        case "w":
+            console.log("前に進む");
+            break;
+        case "a":
+            console.log("左に進む");
+            break;
+        case "d":
+            console.log("右に進む");
+            break;
+        case "s":
+            console.log("後ろに進む");
+            break;
+        default:
+            console.log("進めないよ!");
+    }
     return 0
 }
