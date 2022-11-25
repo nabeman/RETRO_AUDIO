a9a5ac5f8e7a17c0c28fe6dc8136efa40d50d3cc
nabeman <tkmcoins20@gmail.com>
2022-11-25 22:25:40 +0900

    微調整

--- main.js
+++ main.js
@@ -1,35 +1,35 @@
-const input_button = document.getElementById("start_key");
-input_button.addEventListener("click", start_input);
-let state = false;
+// const input_button = document.getElementById("start_key");
+// input_button.addEventListener("click", start_input);
+// let state = false;
 
-function start_input(e){
-    if(!state){
-        document.addEventListener("keypress", keypress, false);
-        state = true;
-        console.log("入力を受け付けています");
-    }else{
-        document.removeEventListener("keypress", keypress, false);
-        state = false;
-        console.log("入力を停止しました");
-    }
-}
+// function start_input(e){
+//     if(!state){
+//         document.addEventListener("keypress", keypress, false);
+//         state = true;
+//         console.log("入力を受け付けています");
+//     }else{
+//         document.removeEventListener("keypress", keypress, false);
+//         state = false;
+//         console.log("入力を停止しました");
+//     }
+// }
 
-function keypress(e){
-    switch(e.key){
-        case "w":
-            console.log("前に進む");
-            break;
-        case "a":
-            console.log("左に進む");
-            break;
-        case "d":
-            console.log("右に進む");
-            break;
-        case "s":
-            console.log("後ろに進む");
-            break;
-        default:
-            console.log("進めないよ!");
-    }
-    return 0
-}
+// function keypress(e){
+//     switch(e.key){
+//         case "w":
+//             console.log("前に進む");
+//             break;
+//         case "a":
+//             console.log("左に進む");
+//             break;
+//         case "d":
+//             console.log("右に進む");
+//             break;
+//         case "s":
+//             console.log("後ろに進む");
+//             break;
+//         default:
+//             console.log("進めないよ!");
+//     }
+//     return 0
+// }
