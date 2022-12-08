6e090f851b07d6d1d7feaabeb8a950e2fc9dbcd9
nabeman <tkmcoins20@gmail.com>
2022-12-08 23:46:09 +0900

    いったんコミットエラー発生中

---
+++ keyaudio.js
@@ -0,0 +1,150 @@
+function key_audio(key, PlayAudio, downScale){
+    if(!downScale){
+        switch(key){
+            case "1":
+                PlayAudio(415);
+                break;
+            case "q":
+                PlayAudio(440);
+                break;
+            case "2":
+                PlayAudio(466);
+                break;
+            case "w":
+                PlayAudio(494);
+                break;
+            case "e":
+                PlayAudio(523.2); //ド5 C5
+                console.log("ド5");
+                break;
+            case "4":
+                PlayAudio(554.3);
+                break;
+            case "r":
+                PlayAudio(587.3);
+                break;
+            case "5":
+                PlayAudio(622.2);
+                break;
+            case "t":
+                PlayAudio(659.2);
+                break;
+            case "y":
+                PlayAudio(698.5);
+                break;
+            case "7":
+                PlayAudio(740);
+                break;
+            case "u":
+                PlayAudio(784);
+                break;
+            case "8":
+                PlayAudio(830.6);
+                break;
+            case "i":
+                PlayAudio(880);
+                break;
+            case "9":
+                PlayAudio(932.3);
+                break;
+            case "o":
+                PlayAudio(987.8);
+                break;
+            case "p":
+                PlayAudio(1046.5); //ド6 C6
+                console.log("ド6");
+                break;
+            case "-":
+                PlayAudio(1108.7); 
+                break;
+            case "@":
+                PlayAudio(1174.6);
+                break;
+            case "^":
+                PlayAudio(1244.5);
+                break;
+            case "[":
+                PlayAudio(1318.5); //ミ6 E6
+                break;
+            case " ":
+                downScale = !downScale;
+                break;
+            case "a":
+                dualAudio(415);
+                break;
+            default:
+                console.log("無効な入力です")
+        }
+    }
+    else{
+        switch(key){
+            case "q":
+                PlayAudio(130.8); //ド3 C3
+                console.log("ド3");
+                break;
+            case "2":
+                PlayAudio(138.6);
+                break;
+            case "w":
+                PlayAudio(146.8);
+                break;
+            case "3":
+                PlayAudio(155.5);
+                break;
+            case "e":
+                PlayAudio(164.8);
+                break;
+            case "r":
+                PlayAudio(174.6);
+                break;
+            case "5":
+                PlayAudio(185);
+                break;
+            case "t":
+                PlayAudio(196);
+                break;
+            case "6":    
+                PlayAudio(207.6);
+                break;
+            case "y":
+                PlayAudio(220);
+                break;
+            case "7":
+                PlayAudio(233);
+                break;
+            case "u":
+                PlayAudio(247);
+                break;
+            case "i":
+                PlayAudio(261.6); //ド4 C4
+                console.log("ド4");
+                break;
+            case "9":
+                PlayAudio(277);
+                break;
+            case "o":
+                PlayAudio(293.6);
+                break;
+            case "0":
+                PlayAudio(311);
+                break;
+            case "p":
+                PlayAudio(329.6);
+                break;
+            case "@":
+                PlayAudio(349);
+                break;
+            case "^":
+                PlayAudio(370);
+                break;
+            case "[":
+                PlayAudio(392);
+                break;
+            case " ":
+                downScale = !downScale;
+                break;
+            default:
+                console.log("無効な入力です")
+        }
+    }
+}
--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const { createApp } = Vue;
+
 const ctx = new AudioContext();
 const gainNode = ctx.createGain();
 gainNode.gain.value = 0.3;
@@ -7,6 +9,8 @@ const app = createApp({
         return{
             isPlaying: false,
             oscillator: null,
+            downScale: false,
+            prep: false
         }
     },
     methods:{
@@ -22,13 +26,29 @@ const app = createApp({
             this.oscillator?.stop(); //oscillator 止める
             this.isPlaying = false;
         },
-        keydown_audio(event){
-            
+        keydown_audio(event){ //keyが押されたときに実行
+            if(this.isPlaying) return;
+            key_audio(event.key, this.PlayAudio, this.downScale);
         },
-        keyup_audio(event){
-            if(isPlaying){
+        keyup_audio(){
+            if(this.isPlaying){ //keyが離れた時に実行
                 this.StopAudio();
+            }
+        },
+        prep_audio(){
+            if(!this.prep){
+                document.addEventListener("keydown", this.keydown_audio, false);
+                document.addEventListener("keyup", this.keyup_audio, false);
+                this.prep = true;
+                console.log("準備が完了しました")
+            }else{
+                document.removeEventListener("keydown", this.keydown_audio, false);
+                document.removeEventListener("keyup", this.keyup_audio, false);
+                this.prep = false;
+                console.log("演奏準備を取り消しました")
             }
         }
     }
 })
+
+app.mount('#main_div')
